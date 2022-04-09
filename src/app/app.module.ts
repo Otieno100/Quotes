fa2c25841a68b067e70525f9ca84117e4a45b03b
Otieno100 <brian.masafu@student.moringaschool.com>
2022-04-09 18:42:45 +0300

    ft: generated new strikethrough directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { StrikethroughDirective } from './strikethrough.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    QuotesComponent
+    QuotesComponent,
+    StrikethroughDirective
   ],
   imports: [
     BrowserModule,
