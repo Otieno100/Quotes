59c46cf6b8c554d96f3908f0fcab3468ad9ca353
Otieno100 <brian.masafu@student.moringaschool.com>
2022-04-10 00:41:25 +0300

    modified the app root for form

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,20 +1,17 @@
 import { identifierName, Quote } from "@angular/compiler";
 
 export class Quotes {
-    id!: number;
-    quotes!: string;
-    upVotes!:number;
-    downVotes!:number;
+    // id!: number;
+    // quotes!: string;
+    // upVotes!:number;
+    // downVotes!:number;
 
 
 
 
     public showDescription: boolean;
-    constructor(id: number, quote:string, upVotes:number, downVotes:number){
-    this.id = id,
-    this.quotes = quote,
-    this.upVotes = upVotes,
-    this. downVotes = downVotes
+    constructor( public id: number,public quotes:string, public upVotes:number, public downVotes:number, public completeDate:Date,){
+  
 
     this.showDescription=false;
     }
