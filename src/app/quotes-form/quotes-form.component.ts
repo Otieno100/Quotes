33b3e3d024ada7fc4d35e78de54b76b0aca21ea0
Otieno100 <brian.masafu@student.moringaschool.com>
2022-04-10 00:39:43 +0300

    modified constructor for form component and date

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,12 +1,16 @@
+import { DatePipe } from '@angular/common';
+import { compileClassMetadata } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+import { Quotes } from '../quotes';
 @Component({
   selector: 'app-quotes-form',
   templateUrl: './quotes-form.component.html',
   styleUrls: ['./quotes-form.component.css']
 })
 export class QuotesFormComponent implements OnInit {
-
+  form=NgForm
+  newQuote = new Quotes(0,"",0,0,Date);
   constructor() { }
 
   ngOnInit(): void {
