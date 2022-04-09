8e8673c1c4c5ce1629ea520dcbd7a5a44872d6c1
Otieno100 <brian.masafu@student.moringaschool.com>
2022-04-09 18:40:13 +0300

    added a directive *ngFor to loop through the array i created

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -2,16 +2,17 @@ import { identifierName, Quote } from "@angular/compiler";
 
 export class Quotes {
     id!: number;
-    quote!: string;
+    quotes!: string;
     upVotes!:number;
     downVotes!:number;
+
 
 
 
     public showDescription: boolean;
     constructor(id: number, quote:string, upVotes:number, downVotes:number){
     this.id = id,
-    this.quote = quote,
+    this.quotes = quote,
     this.upVotes = upVotes,
     this. downVotes = downVotes
 
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,11 +1,24 @@
+// import { Quote } from '@angular/compiler';
+// import { Quote } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-
+import { Quotes } from '../quotes';
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+
+
+  quotes: Quotes[] = [
+    new Quotes(1, 'first Quote', 0,0),
+    new Quotes(2, 'second Quote',0,0),
+    new Quotes(3, 'Third Quote',0,0),
+    new Quotes(4, 'Fourth Quote',0,0),
+    new Quotes(5, 'Fifth Quote',0,0),
+    new Quotes(6,'last Quote',0,0),
+  ];
+
 
   constructor() { }
 
